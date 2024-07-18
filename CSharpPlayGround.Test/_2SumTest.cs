086e9e4522e07f4c100b74b72fc50e00b2d289d6
Vichea <nathvichea1@gmail.com>
2024-07-18 13:55:55 -0500

    Add 2 Sum And git workflow

--- CSharpPlayGround.Test/_2SumTest.cs
+++ CSharpPlayGround.Test/_2SumTest.cs
@@ -16,7 +16,6 @@ public class _2SumTest
         var _2Sum = new _2Sum();
         var result = _2Sum.TwoSum(new int[] { 3, 2, 4 }, 6);
         Assert.Equal(new int[] { 1, 2 }, result);
-
     }
 
     [Fact]
@@ -25,5 +24,12 @@ public class _2SumTest
         var _2Sum = new _2Sum();
         var result = _2Sum.TwoSum(new int[] { 3, 3 }, 6);
         Assert.Equal(new int[] { 0, 1 }, result);
+    }
+    [Fact]
+    public void Case4()
+    {
+        var _2Sum = new _2Sum();
+        var result = _2Sum.TwoSum(new int[] { 3, 2, 3 }, 6);
+        Assert.Equal(new int[] { 0, 2 }, result);
     }
 }
